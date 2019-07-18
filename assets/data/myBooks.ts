6aadbb723f30d8f0328ae4880800791389fecd14
kabire <52547162+kabire@users.noreply.github.com>
2019-07-18 16:39:02 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import {BookInfo} from '../../app/bookInfo';
+
+export const MYBOOKS: BookInfo[] =
+[
+    {bname: 'A Tale Of Two Cities', bauthor: 'Charles Dickens', bgenre: 'Classic', byear: '1958', bpic: 'assets/images/a.JPG'},
+    {bname: 'The Da Vinci Code', bauthor: 'Dan Brown', bgenre: 'Mystery', byear: '1998', bpic: 'assets/images/b.JPG'},
+    {bname: 'Harry Potter And The Deathly Hallows', bauthor: 'J.K.Rowling', bgenre: 'Fiction', byear: '2010', bpic: 'assets/images/c.JPG'},
+    {bname: 'The Adventures Of Tom Sawyer', bauthor: 'Mark Twain', bgenre: 'Mystery', byear: '1978', bpic: 'assets/images/d.JPG'}
+]
